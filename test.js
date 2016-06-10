669815d8e89fa1823ee526d035fc377f6384a38d
Andrea Parodi <andrea@parro.it>
2016-06-11 00:07:17 +0200

    Removed tests that are essentially testing code implemented by nbind

--- test.js
+++ test.js
@@ -1,50 +1,18 @@
 import test from 'ava';
 import libui from './index';
 
+libui.Ui.init();
+
 test('newWindow - throw if too few arguments', t => {
-	const err = t.throws(() => libui.newWindow('Test window', 800, 600));
-	t.true(err instanceof TypeError);
-	t.is(err.message, 'Too few arguments, expected 4');
+	const err = t.throws(() => new libui.UiWindow('Test window'));
+	t.true(err instanceof Error);
+	t.is(err.message, 'Wrong number of arguments');
 });
 
-test('controlShow - throw if too few arguments', t => {
-	const err = t.throws(() => libui.controlShow());
-	t.true(err instanceof TypeError);
-	t.is(err.message, 'Too few arguments, expected 1');
-});
-
-test('controlDestroy - throw if too few arguments', t => {
-	const err = t.throws(() => libui.controlDestroy());
-	t.true(err instanceof TypeError);
-	t.is(err.message, 'Too few arguments, expected 1');
-});
-
-test('windowOnClosing - throw if too few arguments', t => {
-	const err = t.throws(() => libui.windowOnClosing());
-	t.true(err instanceof TypeError);
-	t.is(err.message, 'Too few arguments, expected 2');
-});
-
-test('newWindow - throw if width is not a number', t => {
-	const err = t.throws(() => libui.newWindow('Test window', '', 600, true));
-	t.true(err instanceof TypeError);
+/*
+test.skip('newWindow - throw if width is not a number', t => {
+	const err = t.throws(() => new libui.UiWindow('Test window', 'some text', 600, true));
+	t.true(err instanceof Error);
 	t.is(err.message, 'width should be a number');
 });
-
-test('newWindow - throw if height is not a number', t => {
-	const err = t.throws(() => libui.newWindow('Test window', 600, '', true));
-	t.true(err instanceof TypeError);
-	t.is(err.message, 'height should be a number');
-});
-
-test('newWindow - throw if title is not a string', t => {
-	const err = t.throws(() => libui.newWindow(true, 600, 400, true));
-	t.true(err instanceof TypeError);
-	t.is(err.message, 'title should be a string');
-});
-
-test('newWindow - throw if hasMenubar is not a boolean', t => {
-	const err = t.throws(() => libui.newWindow('Test window', 600, 400, 0));
-	t.true(err instanceof TypeError);
-	t.is(err.message, 'hasMenubar should be a boolean');
-});
+*/
