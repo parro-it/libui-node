ca9b390b702a373760fb7bc0123f83aed0517877
Andrea Parodi <andrea@parro.it>
2016-06-11 00:24:41 +0200

    temporary get rid of ava to verify travis problem

--- test.js
+++ test.js
@@ -1,12 +1,19 @@
-import test from 'ava';
-import libui from './index';
+// import test from 'ava';
+// import libui from './index';
 
+const libui = require('./index');
+
+libui.Ui.init();
+const win = new libui.UiWindow('Test window');
+
+/*
 test('newWindow - throw if too few arguments', t => {
 	libui.Ui.init();
 	const err = t.throws(() => new libui.UiWindow('Test window'));
 	t.true(err instanceof Error);
 	t.is(err.message, 'Wrong number of arguments');
 });
+*/
 
 /*
 test.skip('newWindow - throw if width is not a number', t => {
