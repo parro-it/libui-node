56559a75fcffb0893815da8563db307265ca5df3
Andrea Parodi <andrea@parro.it>
2016-06-11 00:14:40 +0200

    moved libui init insed test method

--- test.js
+++ test.js
@@ -1,9 +1,8 @@
 import test from 'ava';
 import libui from './index';
 
-libui.Ui.init();
-
 test('newWindow - throw if too few arguments', t => {
+	libui.Ui.init();
 	const err = t.throws(() => new libui.UiWindow('Test window'));
 	t.true(err instanceof Error);
 	t.is(err.message, 'Wrong number of arguments');
