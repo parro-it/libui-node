3bfc53aa06e2437aa189d18cfedb4839682e8a3f
Andrea Parodi <andrea@parro.it>
2016-06-13 21:52:49 +0200

    removed es6 syntax from index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var os = require('os');
-const nbind = require('nbind');
-const binding = nbind.init(__dirname);
-const Ui = binding.lib.Ui;
+var nbind = require('nbind');
+var binding = nbind.init(__dirname);
+var Ui = binding.lib.Ui;
 
-let loopRunning = false;
+var loopRunning = false;
+
 module.exports = binding.lib;
 
 function stopLoop() {
@@ -11,22 +12,22 @@ function stopLoop() {
 	Ui.quit();
 }
 
-function startLoop() {
+function startLoop(cb) {
+	function step() {
+		Ui.mainStep(false);
+		if (loopRunning) {
+			setImmediate(step);
+		} else if (cb) {
+			cb();
+		}
+	}
+
 	loopRunning = true;
 	if (os.platform() === 'darwin') {
 		return Ui.main();
 	}
-	return new Promise(resolve => {
-		function step() {
-			Ui.mainStep(false);
-			if (loopRunning) {
-				setImmediate(step);
-			} else {
-				resolve();
-			}
-		}
-		step();
-	});
+
+	step();
 }
 
 module.exports.startLoop = startLoop;
