ffcb8355541527428f9ba7c1f50b45574250ddfb
Andrea Parodi <andrea@parro.it>
2016-06-13 20:50:21 +0200

    removed searchentry from example on OSX

--- examples/core-api.js
+++ examples/core-api.js
@@ -1,21 +1,27 @@
-const libui = require('../index.js');
+var os = require('os');
+var libui = require('../index.js');
+
 libui.Ui.init();
-const win = new libui.UiWindow('Test window', 800, 600, false);
+var win = new libui.UiWindow('Test window', 800, 600, false);
 win.margined = 1;
 
-const box = new libui.UiVerticalBox();
-const hBox = new libui.UiHorizontalBox();
-const e1 = new libui.UiEntry();
+var box = new libui.UiVerticalBox();
+var hBox = new libui.UiHorizontalBox();
+var e1 = new libui.UiEntry();
 e1.enabled = 0;
 hBox.append(new libui.UiLabel('ciao'), 0);
 hBox.append(e1, 1);
 
 box.append(new libui.UiEntry(), 1);
 box.append(hBox, 0);
-box.append(new libui.UiSearchEntry(), 0);
+
+if (os.platform() !== 'darwin') {
+	box.append(new libui.UiSearchEntry(), 0);
+}
+
 box.append(new libui.UiPasswordEntry(), 0);
 
-const cmb = new libui.UiCombobox();
+var cmb = new libui.UiCombobox();
 
 cmb.append('item 1');
 cmb.append('item 2');
@@ -26,8 +32,8 @@ box.append(new libui.UiDateTimePicker(), 1);
 box.append(new libui.UiEditableCombobox(), 1);
 box.append(new libui.UiCheckbox('Optionally'), 1);
 
-const group = new libui.UiGroup('Options');
-const radios = new libui.UiRadioButtons();
+var group = new libui.UiGroup('Options');
+var radios = new libui.UiRadioButtons();
 radios.append('option 1');
 radios.append('option 2');
 radios.append('option 3');
@@ -37,15 +43,15 @@ box.append(group, 1);
 
 box.append(new libui.UiSeparator(), 1);
 
-const hBox2 = new libui.UiHorizontalBox();
-const spin = new libui.UiSpinbox(0, 100);
+var hBox2 = new libui.UiHorizontalBox();
+var spin = new libui.UiSpinbox(0, 100);
 spin.value = 33;
 hBox2.append(new libui.UiLabel('Number'), 0);
 hBox2.append(spin, 1);
 box.append(hBox2, 0);
 
-const hBox3 = new libui.UiHorizontalBox();
-const slider = new libui.UiSlider(0, 100);
+var hBox3 = new libui.UiHorizontalBox();
+var slider = new libui.UiSlider(0, 100);
 slider.value = 33;
 hBox3.append(new libui.UiLabel('Number'), 0);
 hBox3.append(slider, 1);
@@ -53,7 +59,7 @@ box.append(hBox3, 0);
 
 box.append(new libui.UiMultilineEntry(), 0);
 
-const buttons = new libui.UiHorizontalBox();
+var buttons = new libui.UiHorizontalBox();
 buttons.append(new libui.UiButton('Ok'), 0);
 buttons.append(new libui.UiButton('Maybe'), 0);
 buttons.append(new libui.UiButton('Cancel'), 0);
@@ -62,7 +68,7 @@ box.append(new libui.UiProgressBar(), 0);
 
 box.append(buttons, 0);
 
-const tabs = new libui.UiTab();
+var tabs = new libui.UiTab();
 
 tabs.append('controls', box);
 tabs.append('text', new libui.UiMultilineEntry());
