fa071669d5257c0330c93829573ccf6efa6bf49e
Andrea Parodi <andrea@parro.it>
2016-07-05 10:29:16 +0200

    moved arch specific functions to own folder

--- src/EventLoop.cc
+++ src/EventLoop.cc
@@ -1,50 +1,30 @@
 #include "nbind/nbind.h"
-#include <X11/Xlib.h>
-#include <gtk/gtk.h>
-#include <gdk/gdk.h>
-#include <gdk/gdkx.h>
 #include <uv.h>
 #include "../ui.h"
 
 
 void onEventsPending(uv_poll_t* handle, int status, int events) {
-	// printf("status %d, events %d\n", status, events);
+
 	Nan::HandleScope scope;
-	while(gtk_events_pending()) {
+	while(uiEventsPending()) {
 		uiMainStep(0);
 	}
 
 }
+
 uv_poll_t * handle;
-
-
-void quit()
-{
-	printf("gtk_main_quit\n");
-	gtk_main_quit();
-	printf("stopping poll\n");
-	uv_poll_stop(handle);
-	printf("end\n");
-}
 
 struct EventLoop {
 	static void start () {
-		GdkDisplay *pDisplay;
-		Display *xDisplay;
-
-		int fd;
 
 		uiMainSteps();
 
-		pDisplay = gdk_display_get_default();
-		xDisplay = (Display *) gdk_x11_display_get_xdisplay(pDisplay);
-		fd = ConnectionNumber(xDisplay);
+		int fd =uiConnectionNumber();
 
 		handle = new uv_poll_t();
 
 		uv_poll_init(uv_default_loop(), handle, fd);
 		uv_poll_start(handle, UV_READABLE, onEventsPending);
-
 	}
 
 	static void stop () {
