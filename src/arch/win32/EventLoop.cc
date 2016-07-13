58d84f31d348876d7a3db45636b34f7431792c2f
Andrea Parodi <andrea@parro.it>
2016-07-13 21:09:23 +0200

    hook WIP

--- src/arch/win32/EventLoop.cc
+++ src/arch/win32/EventLoop.cc
@@ -1,8 +1,25 @@
 #include "../../../ui.h"
 #include "nbind/nbind.h"
+#include <uv.h>
+// #include <Windows.h>
 
 bool running = false;
+/*uv_thread_t *thread;
 
+LRESULT CALLBACK onEvents(int nCode, WPARAM wParam, LPARAM lParam) {
+	printf("%d\n", nCode);
+}
+
+
+void pollEvents(void* threadId) {
+	SetWindowsHookEx(
+  		WH_CALLWNDPROC,
+  		onEvents,
+  		NULL,
+  		threadId
+	);
+}
+*/
 struct EventLoop {
 	static void start () {
 		if (running) {
@@ -10,6 +27,9 @@ struct EventLoop {
 		}
 
 		running = true;
+		//thread = new uv_thread_t();
+		//int threadId = GetCurrentThreadId();
+		//uv_thread_create(thread, pollEvents, threadId);
 		uiMain();
 	}
 
