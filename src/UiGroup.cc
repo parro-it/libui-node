9b3a2d9a44bf6888ec3d53b9a1808c246180d235
Andrea Parodi <andrea@parro.it>
2018-04-07 08:32:45 +0200

    std::string --> UiGroup

--- src/UiGroup.cc
+++ src/UiGroup.cc
@@ -1,46 +1,51 @@
+#include <string>
 #include "../ui.h"
 #include "ui-node.h"
 #include "nbind/api.h"
 
 class UiGroup : public UiControl {
   public:
-	UiGroup(const char *text);
+	UiGroup(std::string text);
 	UiGroup();
 	void setChild(UiControl *control);
 	bool getMargined();
 	void setMargined(bool margined);
-	const char *getTitle();
-	void setTitle(const char *title);
+	std::string getTitle();
+	void setTitle(std::string title);
 	DEFINE_CONTROL_METHODS()
 };
 
-UiGroup::UiGroup(const char *text) : UiControl((uiControl *)uiNewGroup(text)) {}
-UiGroup::UiGroup() : UiControl((uiControl *)uiNewGroup("")) {}
+UiGroup::UiGroup(std::string text)
+	: UiControl(uiControl(uiNewGroup(text.c_str()))) {}
+UiGroup::UiGroup() : UiControl(uiControl(uiNewGroup(""))) {}
 
 void UiGroup::setChild(UiControl *control) {
-	uiGroupSetChild((uiGroup *)getHandle(), control->getHandle());
+	uiGroupSetChild(uiGroup(getHandle()), control->getHandle());
 }
 
 bool UiGroup::getMargined() {
-	return uiGroupMargined((uiGroup *)getHandle());
+	return uiGroupMargined(uiGroup(getHandle()));
 }
 
 void UiGroup::setMargined(bool margined) {
-	uiGroupSetMargined((uiGroup *)getHandle(), margined);
+	uiGroupSetMargined(uiGroup(getHandle()), margined);
 }
 
-const char *UiGroup::getTitle() {
-	return uiGroupTitle((uiGroup *)getHandle());
+std::string UiGroup::getTitle() {
+	char *char_ptr = uiGroupTitle(uiGroup(getHandle()));
+	std::string s(char_ptr);
+	uiFreeText(char_ptr);
+	return s;
 }
 
-void UiGroup::setTitle(const char *title) {
-	uiGroupSetTitle((uiGroup *)getHandle(), title);
+void UiGroup::setTitle(std::string title) {
+	uiGroupSetTitle(uiGroup(getHandle()), title.c_str());
 }
 
 INHERITS_CONTROL_METHODS(UiGroup)
 
 NBIND_CLASS(UiGroup) {
-	construct<const char *>();
+	construct<std::string>();
 	construct<>();
 	method(setChild);
 	method(getTitle);
