af9650a01e8ea181e9dd7c9d19ff2fcd84d1282b
Andrea Parodi <andrea@parro.it>
2018-04-13 11:21:09 +0200

    Fixed events leak -> UiCheckbox

--- src/UiCheckbox.cc
+++ src/UiCheckbox.cc
@@ -18,12 +18,13 @@ class UiCheckbox : public UiControl {
 	void onDestroy(uiControl *control) override;
 };
 
-void UiCheckbox::onDestroy(uiControl *control){
+void UiCheckbox::onDestroy(uiControl *control) {
 	/*
 		freeing event callbacks to allow JS to garbage collect this class
 		when there are no references to it left in JS code.
 	*/
-	DISPOSE_EVENT(onToggled)}
+	DISPOSE_EVENT(onToggled);
+}
 
 UiCheckbox::~UiCheckbox() {
 	printf("UiCheckbox %p destroyed with wrapper %p.\n", getHandle(), this);
