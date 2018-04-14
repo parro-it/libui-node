21e259746bde3e035a842b5ea0ddad2c50fb2139
Andrea Parodi <andrea@parro.it>
2018-04-14 09:04:36 +0200

    removed macro -> UiDateTimePicker

--- src/UiDateTimePicker.cc
+++ src/UiDateTimePicker.cc
@@ -5,45 +5,36 @@
 class UiDateTimePicker : public UiControl {
   public:
 	UiDateTimePicker();
-	DEFINE_CONTROL_METHODS()
 };
 
 UiDateTimePicker::UiDateTimePicker()
 	: UiControl((uiControl *)uiNewDateTimePicker()) {}
 
-INHERITS_CONTROL_METHODS(UiDateTimePicker)
-
 NBIND_CLASS(UiDateTimePicker) {
+	inherit(UiControl);
 	construct<>();
-	DECLARE_CHILD_CONTROL_METHODS()
 }
 
 class UiTimePicker : public UiControl {
   public:
 	UiTimePicker();
-	DEFINE_CONTROL_METHODS()
 };
 
 UiTimePicker::UiTimePicker() : UiControl((uiControl *)uiNewTimePicker()) {}
 
-INHERITS_CONTROL_METHODS(UiTimePicker)
-
 NBIND_CLASS(UiTimePicker) {
+	inherit(UiControl);
 	construct<>();
-	DECLARE_CHILD_CONTROL_METHODS()
 }
 
 class UiDatePicker : public UiControl {
   public:
 	UiDatePicker();
-	DEFINE_CONTROL_METHODS()
 };
 
 UiDatePicker::UiDatePicker() : UiControl((uiControl *)uiNewDatePicker()) {}
 
-INHERITS_CONTROL_METHODS(UiDatePicker)
-
 NBIND_CLASS(UiDatePicker) {
+	inherit(UiControl);
 	construct<>();
-	DECLARE_CHILD_CONTROL_METHODS()
 }
