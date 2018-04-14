39a44c64fb9f4ff41227449c31ae14cbaac07160
Andrea Parodi <andrea@parro.it>
2018-04-14 10:40:00 +0200

    Removed macro from header

--- src/includes/control.h
+++ src/includes/control.h
@@ -26,61 +26,6 @@
 		NAME##Callback = nullptr;                                              \
 	}
 
-#define DEFINE_CONTROL_METHODS()                                               \
-	void destroy();                                                            \
-	void setParent(UiControl *parent);                                         \
-	bool toplevel();                                                           \
-	bool getVisible();                                                         \
-	void setVisible(bool visible);                                             \
-	bool getEnabled();                                                         \
-	void setEnabled(bool enabled);
-
-#define INHERITS_CONTROL_METHODS(CLASS)                                        \
-	void CLASS::destroy() {                                                    \
-		UiControl::destroy();                                                  \
-	}                                                                          \
-	void CLASS::setParent(UiControl *parent) {                                 \
-		UiControl::setParent(parent);                                          \
-	}                                                                          \
-	bool CLASS::toplevel() {                                                   \
-		return UiControl::toplevel();                                          \
-	}                                                                          \
-	bool CLASS::getVisible() {                                                 \
-		return UiControl::getVisible();                                        \
-	}                                                                          \
-	void CLASS::setVisible(bool visible) {                                     \
-		UiControl::setVisible(visible);                                        \
-	}                                                                          \
-	bool CLASS::getEnabled() {                                                 \
-		return UiControl::getEnabled();                                        \
-	}                                                                          \
-	void CLASS::setEnabled(bool enabled) {                                     \
-		UiControl::setEnabled(enabled);                                        \
-	}
-
-#define DECLARE_CHILD_CONTROL_METHODS()                                        \
-	inherit(UiControl);                                                        \
-	method(destroy);                                                           \
-	method(setParent);                                                         \
-	method(toplevel);                                                          \
-	method(getVisible);                                                        \
-	method(setVisible);                                                        \
-	method(getEnabled);                                                        \
-	method(setEnabled);                                                        \
-	getset(getVisible, setVisible);                                            \
-	getset(getEnabled, setEnabled);
-
-#define DECLARE_CONTROL_METHODS()                                              \
-	method(destroy);                                                           \
-	method(setParent);                                                         \
-	method(toplevel);                                                          \
-	method(getVisible);                                                        \
-	method(setVisible);                                                        \
-	method(getEnabled);                                                        \
-	method(setEnabled);                                                        \
-	getset(getVisible, setVisible);                                            \
-	getset(getEnabled, setEnabled);
-
 typedef void (*DestroyCb)(uiControl *);
 
 class UiControl {
@@ -95,8 +40,13 @@ class UiControl {
 	uiControl *getHandle();
 	UiControl(uiControl *hnd);
 	virtual ~UiControl();
-
-	DEFINE_CONTROL_METHODS()
+	void destroy();
+	void setParent(UiControl *parent);
+	bool toplevel();
+	bool getVisible();
+	void setVisible(bool visible);
+	bool getEnabled();
+	void setEnabled(bool enabled);
 };
 
 // This is included at end of file
