a196d64e241f33d11eb6673279dbd81e35b87dc6
Andrea Parodi <andrea@parro.it>
2018-04-14 10:49:34 +0200

    UiEntry

--- src/UiEntry.cc
+++ src/UiEntry.cc
@@ -1,7 +1,6 @@
 #include <string>
 #include "nbind/api.h"
 #include "control.h"
-#include "entry.h"
 #include "ui.h"
 
 class UiEntryBase : public UiControl {
@@ -10,7 +9,10 @@ class UiEntryBase : public UiControl {
 
   public:
 	UiEntryBase(uiControl *, const char *name);
-	DEFINE_ENTRY_METHODS()
+	void setText(std::string text);
+	std::string getText();
+	void setReadOnly(bool readOnly);
+	bool getReadOnly();
 	~UiEntryBase();
 	void onDestroy(uiControl *control) override;
 };
@@ -30,21 +32,18 @@ void UiEntryBase::onDestroy(uiControl *control) {
 class UiEntry : public UiEntryBase {
   public:
 	UiEntry();
-	DEFINE_ENTRY_METHODS()
 	void onChanged(nbind::cbFunction &cb);
 };
 
 class UiPasswordEntry : public UiEntryBase {
   public:
 	UiPasswordEntry();
-	DEFINE_ENTRY_METHODS()
 	void onChanged(nbind::cbFunction &cb);
 };
 
 class UiSearchEntry : public UiEntryBase {
   public:
 	UiSearchEntry();
-	DEFINE_ENTRY_METHODS()
 	void onChanged(nbind::cbFunction &cb);
 };
 
@@ -78,32 +77,34 @@ IMPLEMENT_EVENT(UiEntryBase, uiEntry, onChanged, uiEntryOnChanged)
 
 UiEntry::UiEntry() : UiEntryBase(uiControl(uiNewEntry()), "UiEntry") {}
 
-INHERITS_ENTRY_METHODS(UiEntry)
-
 UiPasswordEntry::UiPasswordEntry()
 	: UiEntryBase(uiControl(uiNewPasswordEntry()), "UiPasswordEntry") {}
-
-INHERITS_ENTRY_METHODS(UiPasswordEntry)
 
 UiSearchEntry::UiSearchEntry()
 	: UiEntryBase(uiControl(uiNewSearchEntry()), "UiSearchEntry") {}
 
-INHERITS_ENTRY_METHODS(UiSearchEntry)
+NBIND_CLASS(UiEntryBase) {
+	inherit(UiControl);
+	getset(getText, setText);
+	getset(getReadOnly, setReadOnly);
+	method(onChanged);
+	method(getText);
+	method(setText);
+	method(getReadOnly);
+	method(setReadOnly);
+}
 
 NBIND_CLASS(UiSearchEntry) {
-	inherit(UiControl);
+	inherit(UiEntryBase);
 	construct<>();
-	DECLARE_ENTRY_METHODS()
 }
 
 NBIND_CLASS(UiPasswordEntry) {
-	inherit(UiControl);
+	inherit(UiEntryBase);
 	construct<>();
-	DECLARE_ENTRY_METHODS()
 }
 
 NBIND_CLASS(UiEntry) {
-	inherit(UiControl);
+	inherit(UiEntryBase);
 	construct<>();
-	DECLARE_ENTRY_METHODS()
 }
