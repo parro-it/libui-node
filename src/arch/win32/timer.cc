e10094ba5421b69acd1cd6611c561a78beb8fa01
Andrea Parodi <andrea@parro.it>
2018-04-15 11:40:36 +0200

    timer with basic syntax [win32]

---
+++ src/arch/win32/timer.cc
@@ -0,0 +1,49 @@
+#include <map>
+#include "nbind/api.h"
+#include "timer.h"
+
+static std::map<UINT_PTR, TimeoutHandle *> timersMap;
+
+void CALLBACK win_timeout_cb(HWND hwnd, UINT uMsg, UINT_PTR idEvent,
+							 DWORD dwTime) {
+	CALL_JSCB(timeoutHandle);
+	KillTimer(utilWindow, timeoutHandle->handle);
+	timersMap.erase(timeoutHandle->handle);
+	timeoutHandle->destroy();
+}
+
+void CALLBACK win_interval_cb(HWND hwnd, UINT uMsg, UINT_PTR idEvent,
+							  DWORD dwTime) {
+	CALL_JSCB(timeoutHandle);
+}
+
+TimeoutHandle *setTimeout(nbind::cbFunction &cb, unsigned int timeout) {
+	nbind::cbFunction *callbackJs = new nbind::cbFunction(cb);
+	TimeoutHandle *timeoutHandle = new TimeoutHandle(callbackJs);
+	timeoutHandle->handle = SetTimer(utilWindow, NULL, timeout, win_timeout_cb);
+	timersMap[timeoutHandle->handle] = timeoutHandle;
+	return timeoutHandle;
+}
+
+void clearTimeout(TimeoutHandle *timeoutHandle) {
+	if (!timeoutHandle->destroyed) {
+		KillTimer(utilWindow, timeoutHandle->handle);
+		timersMap.erase(timeoutHandle->handle);
+		timeoutHandle->destroy();
+	}
+}
+
+TimeoutHandle *setInterval(nbind::cbFunction &cb, unsigned int timeout) {
+	nbind::cbFunction *callbackJs = new nbind::cbFunction(cb);
+	TimeoutHandle *timeoutHandle = new TimeoutHandle(callbackJs);
+	timeoutHandle->handle =
+		SetTimer(utilWindow, NULL, timeout, win_interval_cb);
+	timersMap[timeoutHandle->handle] = timeoutHandle;
+	return timeoutHandle;
+}
+
+void clearInterval(TimeoutHandle *timeoutHandle) {
+	KillTimer(utilWindow, timeoutHandle->handle);
+	timersMap.erase(timeoutHandle->handle);
+	timeoutHandle->destroy();
+}
