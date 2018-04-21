5d841126678a6ba2344d3035aeb99464cacb8045
Andrea Parodi <andrea@parro.it>
2018-04-21 21:10:49 +0200

    Enabled aync hook only on startLoop

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const nbind = require('nbind');
+const async_hooks = require('async_hooks');
 
 const binding = nbind.init(__dirname);
 
@@ -36,6 +37,15 @@ function startLoop() {
 		return;
 	}
 
+	// Create a new AsyncHook instance. All of these callbacks are optional.
+	const asyncHook =
+		async_hooks.createHook({init /*, before, after, destroy, promiseResolve*/});
+
+	// Allow callbacks of this AsyncHook instance to call. This is not an implicit
+	// action after running the constructor, and must be explicitly run to begin
+	// executing callbacks.
+	asyncHook.enable();
+
 	setTimeoutNode = global.setTimeout;
 	global.setTimeout = function(cb, t) {
 		const args = Array.prototype.slice.call(arguments, 2);
@@ -58,6 +68,39 @@ function startLoop() {
 	clearIntervalNode = global.clearInterval;
 	global.clearInterval = binding.lib.clearInterval;
 }
+
+// init is called during object construction. The resource may not have
+// completed construction when this callback runs, therefore all fields of the
+// resource referenced by "asyncId" may not have been populated.
+function init(asyncId, type, triggerAsyncId, resource) {
+	// console.log('init', asyncId);
+	binding.lib.EventLoop.wakeupBackgroundThread();
+}
+/*
+// before is called just before the resource's callback is called. It can be
+// called 0-N times for handles (e.g. TCPWrap), and will be called exactly 1
+// time for requests (e.g. FSReqWrap).
+function before(asyncId) {
+	console.log('before', asyncId);
+}
+
+// after is called just after the resource's callback has finished.
+function after(asyncId) {
+	console.log('after', asyncId);
+}
+
+// destroy is called when an AsyncWrap instance is destroyed.
+function destroy(asyncId) {
+	console.log('destroy', asyncId);
+}
+
+// promiseResolve is called only for promise resources, when the
+// `resolve` function passed to the `Promise` constructor is invoked
+// (either directly or through other means of resolving a promise).
+function promiseResolve(asyncId) {
+	console.log('promiseResolve', asyncId);
+}
+*/
 
 function Color(r, g, b, a) {
 	this.r = r;
@@ -367,56 +410,6 @@ const extKeys = {
 	nMultiply: 38,
 	nDivide: 39
 };
-
-const async_hooks = require('async_hooks');
-
-// Return the ID of the current execution context.
-const eid = async_hooks.executionAsyncId();
-
-// Return the ID of the handle responsible for triggering the callback of the
-// current execution scope to call.
-const tid = async_hooks.triggerAsyncId();
-
-// Create a new AsyncHook instance. All of these callbacks are optional.
-const asyncHook =
-	async_hooks.createHook({init /*, before, after, destroy, promiseResolve*/});
-
-// Allow callbacks of this AsyncHook instance to call. This is not an implicit
-// action after running the constructor, and must be explicitly run to begin
-// executing callbacks.
-asyncHook.enable();
-
-// init is called during object construction. The resource may not have
-// completed construction when this callback runs, therefore all fields of the
-// resource referenced by "asyncId" may not have been populated.
-function init(asyncId, type, triggerAsyncId, resource) {
-	// console.log('init', asyncId);
-	binding.lib.EventLoop.wakeupBackgroundThread();
-}
-
-// before is called just before the resource's callback is called. It can be
-// called 0-N times for handles (e.g. TCPWrap), and will be called exactly 1
-// time for requests (e.g. FSReqWrap).
-function before(asyncId) {
-	console.log('before', asyncId);
-}
-
-// after is called just after the resource's callback has finished.
-function after(asyncId) {
-	console.log('after', asyncId);
-}
-
-// destroy is called when an AsyncWrap instance is destroyed.
-function destroy(asyncId) {
-	console.log('destroy', asyncId);
-}
-
-// promiseResolve is called only for promise resources, when the
-// `resolve` function passed to the `Promise` constructor is invoked
-// (either directly or through other means of resolving a promise).
-function promiseResolve(asyncId) {
-	console.log('promiseResolve', asyncId);
-}
 
 module.exports.textStretch = textStretch;
 module.exports.textItalic = textItalic;
