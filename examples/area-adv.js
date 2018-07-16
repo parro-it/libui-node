20ea5cd7c18c4014c71204209a9180263fa6abf6
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2018-07-16 18:29:32 +0200

    Borderless + area example

--- examples/area-adv.js
+++ examples/area-adv.js
@@ -2,9 +2,15 @@
 /* eslint-disable unicorn/number-literal-case */
 const libui = require('..');
 
+const RESIZE_HANDLE_WIDTH = 8;
+
 const redBrush = new libui.DrawBrush();
 redBrush.type = libui.brushType.solid;
 redBrush.color = new libui.Color(1, 0, 0, 1);
+
+const greyBrush = new libui.DrawBrush();
+greyBrush.type = libui.brushType.solid;
+greyBrush.color = new libui.Color(0.6, 0.6, 0.6, 1);
 
 const blueBrush = new libui.DrawBrush();
 blueBrush.type = libui.brushType.solid;
@@ -17,14 +23,14 @@ console.log(dashedStroke.dashes);
 
 const linearBrush = new libui.DrawBrush();
 linearBrush.type = libui.brushType.linearGradient;
-linearBrush.start = new libui.Point(0, 0);
+linearBrush.start = new libui.Point(50, 50);
 linearBrush.end = new libui.Point(200, 200);
 linearBrush.setStops([new libui.BrushGradientStop(0, new libui.Color(1, 0, 0, 1)), new libui.BrushGradientStop(1, new libui.Color(0, 1, 0, 1))]);
 
 const radialBrush = new libui.DrawBrush();
 radialBrush.type = libui.brushType.radialGradient;
-radialBrush.start = new libui.Point(250, 300);
-radialBrush.end = new libui.Point(250, 300);
+radialBrush.start = new libui.Point(300, 330);
+radialBrush.end = new libui.Point(300, 330);
 radialBrush.outerRadius = 40;
 radialBrush.setStops([new libui.BrushGradientStop(0, new libui.Color(0, 0, 1, 1)), new libui.BrushGradientStop(1, new libui.Color(0.5, 0.5, 1, 1))]);
 console.log(radialBrush.getStops());
@@ -33,12 +39,15 @@ const matrix = new libui.UiDrawMatrix();
 matrix.setIdentity();
 matrix.rotate(70, 280, (Math.PI / 180) * 45)
 
-const onMoveHandle = (x, y) => (330 <= x && x <= 350 && 0 <= y && y <= 20);
-const onResizeHandle = (x, y, p) => (0 <= x && x <= p.getAreaWidth() && (p.getAreaHeight() - 20) <= y && y <= p.getAreaHeight());
+const onCloseHandle = (x, y, p) => (3 <= x && x <= 19 && 2 <= y && y <= 18);
+const onMoveHandle = (x, y, p) => (0 <= x && x <= p.getAreaWidth() && 0 <= y && y <= 20);
+const onResizeBottomHandle = (x, y, p) => ((p.getAreaHeight() - RESIZE_HANDLE_WIDTH) <= y && y <= p.getAreaHeight());
+const onResizeRightHandle = (x, y, p) => (p.getAreaWidth() - RESIZE_HANDLE_WIDTH <= x && x <= p.getAreaWidth() && 20 < y);
+const onResizeLeftHandle = (x, y, p) => (0 <= x && x <= RESIZE_HANDLE_WIDTH && 20 < y);
 
 function handlerDraw(area, p) {
 	let path = new libui.UiDrawPath(libui.fillMode.winding);
-	path.addRectangle(0, 0, 200, 200);
+	path.addRectangle(50, 50, 200, 200);
 	path.end();
 	p.getContext().fill(path, linearBrush);
 	path.freePath();
@@ -47,23 +56,29 @@ function handlerDraw(area, p) {
 
 	// Circle
 	path = new libui.UiDrawPath(libui.fillMode.winding);
-	path.newFigure(0, 0);
-	path.arcTo(250, 300, 50, 0, 2 * Math.PI, false);
+	path.newFigureWithArc(300, 330, 50, 0, 2 * Math.PI, false);
 	path.end();
 	p.getContext().fill(path, radialBrush);
 	path.freePath();
 
 	// Dashed line
 	path = new libui.UiDrawPath(libui.fillMode.winding);
-	path.newFigure(250, 20);
-	path.lineTo(300, 150);
+	path.newFigure(300, 50);
+	path.lineTo(350, 250);
 	path.end();
 	p.getContext().stroke(path, redBrush, dashedStroke);
 	path.freePath();
 
 	// Move handle
 	path = new libui.UiDrawPath(libui.fillMode.winding);
-	path.addRectangle(330, 0, 20, 20);
+	path.addRectangle(0, 0, p.getAreaWidth(), 20);
+	path.end();
+	p.getContext().fill(path, greyBrush);
+	path.freePath();
+
+	// Close button
+	path = new libui.UiDrawPath(libui.fillMode.winding);
+	path.addRectangle(3, 2, 16, 16);
 	path.end();
 	p.getContext().fill(path, redBrush);
 	path.freePath();
@@ -72,7 +87,7 @@ function handlerDraw(area, p) {
 	// Rotated square
 	path = new libui.UiDrawPath(libui.fillMode.winding);
 	p.getContext().transform(matrix);
-	path.addRectangle(20, 230, 100, 100);
+	path.addRectangle(90, 240, 100, 100);
 	path.end();
 	p.getContext().fill(path, redBrush);
 	path.freePath();
@@ -80,7 +95,12 @@ function handlerDraw(area, p) {
 
 	// Resize handle
 	path = new libui.UiDrawPath(libui.fillMode.winding);
-	path.addRectangle(0, p.getAreaHeight() - 20, p.getAreaWidth(), 20);
+	// bottom
+	path.addRectangle(0, p.getAreaHeight() - RESIZE_HANDLE_WIDTH, p.getAreaWidth(), RESIZE_HANDLE_WIDTH);
+	// right
+	path.addRectangle(p.getAreaWidth() - RESIZE_HANDLE_WIDTH, 20, RESIZE_HANDLE_WIDTH, p.getAreaHeight());
+	// left
+	path.addRectangle(0, 20, RESIZE_HANDLE_WIDTH, p.getAreaHeight());
 	path.end();
 	p.getContext().fill(path, blueBrush);
 	path.freePath();
@@ -89,22 +109,38 @@ function handlerDraw(area, p) {
 function mouseEvent(area, evt) {
 	const x = evt.getX(), y = evt.getY();
 
-	if(evt.getDown() === 1) {
-		if (onMoveHandle(x, y)) {
+	if (evt.getDown() === 1) {
+		if (onCloseHandle(x, y, evt)) {
+			mainwin.close();
+			libui.stopLoop();
+		} else if (onMoveHandle(x, y, evt)) {
 			area.beginWindowMove();
-		}
-
-		if (onResizeHandle(x, y, evt)) {
-			area.beginWindowResize(libui.resizeEdge.bottom);
+		} else {
+			const left = onResizeLeftHandle(x, y, evt);
+			const right = onResizeRightHandle(x, y, evt);
+			const bottom = onResizeBottomHandle(x, y, evt);
+			if (bottom && right) {
+				area.beginWindowResize(libui.resizeEdge.bottomRight);
+			} else if (bottom && left) {
+				area.beginWindowResize(libui.resizeEdge.bottomLeft);
+			} else if (right) {
+				area.beginWindowResize(libui.resizeEdge.right);
+			} else if (left) {
+				area.beginWindowResize(libui.resizeEdge.left);
+			} else if (bottom) {
+				area.beginWindowResize(libui.resizeEdge.bottom);
+			}
 		}
 	}
 }
 
 function noop() {}
 
+let mainwin;
+
 function main() {
-	const mainwin = new libui.UiWindow('libui textDrawArea Example', 400, 430, 1);
-	mainwin.margined = true;
+	mainwin = new libui.UiWindow('libui textDrawArea Example', 400, 430, 1);
+	mainwin.borderless = true;
 	mainwin.onClosing(() => {
 		mainwin.close();
 		libui.stopLoop();
